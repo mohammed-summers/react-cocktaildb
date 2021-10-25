4436c1479478e00154d75e6669ca3156bac8acf6
Mohammed <mohammed.k.summers@gmail.com>
2021-10-24 20:20:48 -0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt rerum
+        officia quos quisquam odit quam facilis, sunt quas, iure hic earum
+        tempore excepturi et similique cupiditate, quis itaque officiis
+        exercitationem!
+      </p>
+    </section>
+  );
+};
+
+export default About;
